af43badca01161dddba8847398091dd9f3c74023
Muthu <muthu@egbsystems.com>
2018-09-11 19:41:42 +0530

    added Chart

--- app/views/stock/index.blade.php
+++ app/views/stock/index.blade.php
@@ -12,18 +12,28 @@ exit;
 ?>
 <table class="table table-striped table-hover">
     <thead>
-    <tr><th>No#</th><th>Name</th>
+    <tr><th>No#</th>
+    <th>Action</th>
+    <th>Name</th>
         <th>Price</th>
         <th>Close1</th>
         <th>Close2</th>
         <th>Close3</th>
         <th>Close4</th>
         <th>Close5</th>
-        <th>Total</th><th>Avg</th></tr>
+        <th>Total</th>
+        <th>Avg</th>
+        </tr>
     </thead>
     <tbody>
     <?php foreach($stocks as $stock) {
-        echo "<tr><td><a href='https://www.edelweiss.in$stock->URL' target='_blank'>$stock->id</td>
+        echo "<tr>
+        <td><a href='https://www.edelweiss.in$stock->URL' target='_blank'>$stock->id</td>
+        <td>
+            <a href='https://mmb.moneycontrol.com$stock->mmbURL' target='_blank'>MMB</a>
+            <a href='https://in.tradingview.com/symbols/NSE-$stock->nse/technicals/' target='_blank'>TV</a>
+            <a href='https://in.tradingview.com/chart/?symbol=NSE%3A$stock->nse' target='_blank'>Chat</a>
+        </td>
         <td><a href='".url('market/stock/'.$stock->nse)."' target='_blank'>$stock->nse</a></td>
         <td><a href='http://www.moneycontrol.com/stocks/cptmarket/compsearchnew.php?search_data=&cid=&mbsearch_str=&topsearch_type=1&search_str=".$stock->isin."' target='_blank'>".round($stock->price,2)."</a></td>
         <td>".number_format((float)$stock->closediff1, 2, '.', '')."</td>
