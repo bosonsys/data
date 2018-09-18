d620ad10a9fba4fc32fb6711a4c8f45d931fcb74
Kiruthika <kiruthika@egbsystems.com>
2018-09-18 13:40:51 +0530

    BREAKOUT ADDED

---
+++ app/views/breakout/5days.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.index')
+
+@section('content')
+    <table class="table table-striped table-hover">
+        <tr>
+            <th>#</th>
+            <th>Edel Code</th>
+            <th>NSE Code</th>
+            <th>Edel ID</th>
+            <th>Date</th>
+        </tr>
+    </table>
+
+@stop
