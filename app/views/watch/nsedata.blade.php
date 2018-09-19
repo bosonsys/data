daf06a521c3a6acddce37047e4fc9b22f179e1e5
Muthu <muthu@egbsystems.com>
2018-09-19 19:58:02 +0530

    ETG

--- app/views/watch/nsedata.blade.php
+++ app/views/watch/nsedata.blade.php
@@ -4,7 +4,8 @@
     setInterval(getData, (30 * 1000));
     function getData() {
         var jsonData = $.ajax({
-            url: "http://localhost/market/public/store/nsedata",
+            // url: "http://localhost/market/public/store/nsedata",
+            url: "http://localhost/market/public/marketwatch/ETG500",
             dataType: "json",
             async: false
         }).responseText;
