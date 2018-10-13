45b8420ff290139fc4752984d20c18b3155d8b7a
Kiruthika <kiruthika@egbsystems.com>
2018-10-13 19:22:39 +0530

    changes in dashboard

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -23,39 +23,67 @@ class DashboardController extends \BaseController {
     
     public function lastday()
 	{
-		$lists = DB::table('csvdata')->select('TIMESTAMP')->distinct('TIMESTAMP')
+		$lastDate = DB::table('csvdata')->select('TIMESTAMP')->distinct('TIMESTAMP')
 		->take(1)->orderby('TIMESTAMP','DESC')->get();
-        $arr = array();
-        foreach ($lists as $key => $list) {
-            array_push($arr,$list->TIMESTAMP);
-        }
+		$lastDate = $lastDate[0]->TIMESTAMP;
+
 		$pos = DB::table('csvdata')
-				->select('SYMBOL as n', 'CLOSEP as c', 'TIMESTAMP as t', 'SERIES as s')
+				->select('SYMBOL as n', 'CLOSE as c', 'TIMESTAMP as t')
 				->whereIn('SERIES', ['EQ', 'BE'])
-				->whereIn('TIMESTAMP',$arr)
-				->groupBy('SYMBOL')
-				->orderby('CLOSEP','DESC')
-				->take(10)
+				->where('TIMESTAMP',$lastDate)
+				->orderby('CLOSE','DESC')
+				->take(15)
 				->get();
 		$neg = DB::table('csvdata')
-				->select('SYMBOL as s', 'CLOSEP as p', 'TIMESTAMP as d', 'SERIES as e')
+				->select('SYMBOL as s', 'CLOSE as p', 'TIMESTAMP as d')
 				->whereIn('SERIES', ['EQ', 'BE'])
-				->whereIn('TIMESTAMP',$arr)
+				->where('TIMESTAMP',$lastDate)
 				->groupBy('SYMBOL')
-				->orderby('CLOSEP','ASC')
+				->orderby('CLOSE','ASC')
 				->take(10)
 				->get();
 
-		$date = date('Y-m-d');	
-		$today = DB::table('csvdata')
-		    ->select('SYMBOL', 'CLOSEP', 'TIMESTAMP', 'SERIES')
-		    ->whereIn('SERIES', ['EQ', 'BE'])
-			->where('TIMESTAMP',$date)
-			->orderby('CLOSEP','DESC')
-			->take(10)
-			->get();
-		echo "<pre>"; print_r($today);exit();		
-		return View::make('dashboard.dashboard')->with('lists',$arr)->with('pos',$pos)->with('neg',$neg)->with('today',$today);
-         //return json_encode($stock);
+    $pWeek = date( 'Y-m-d', strtotime( $lastDate . ' -1 week' ) );
+
+		$last5days = DB::table('csvdata')
+						->select('SYMBOL', 'CLOSE', 'TIMESTAMP')
+						->whereIn('SERIES', ['EQ', 'BE'])
+						->where('TIMESTAMP', $pWeek)
+						->orderBy('TIMESTAMP', 'DESC')
+						->orderby('CLOSE','DESC')
+						->take(15)
+						->get();
+		// echo "<pre>"; print_r($last5days); exit();
+		foreach($pos as $key => $v)
+		{
+			$comp = $v->n;
+			foreach ($last5days as $ckey => $cv) {
+			// echo "<br>---".$cv->SYMBOL;
+				if ($comp == $cv->SYMBOL) {
+					$v->cvalue = $cv->CLOSE;
+					$v->per = $this->getPercentageChange($v->cvalue, $v->c);
+					$data_per[$key] = $v->per;
+				}
+			}
+			//   echo number_format(($per), 2);
+			//   echo "<pre>"; print($per); exit();
+		}
+        array_multisort($data_per, SORT_DESC, $pos);
+		echo "<pre>";
+		$top = array_slice($pos, 0, 5);
+		array_multisort($data_per, SORT_ASC, $pos);
+		$last = array_slice($pos, 0, 5);
+		print_r($top); 
+		print_r($last); 
+		// arsort($pos);
+		// print_r($pos); 
+		exit;
+		return View::make('dashboard.dashboard')->with('lastDate',$lastDate)->with('pos',$pos)->with('neg',$neg);
+		 //return json_encode($stock);
+	}
+
+	function getPercentageChange($oldNumber, $newNumber){
+		$decreaseValue = $oldNumber - $newNumber;
+		return ($decreaseValue / $oldNumber) * 100;
 	}
 }
