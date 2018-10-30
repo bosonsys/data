0e5859091dcc036e4ff9e2e9390f3dd8842ffd34
Muthu <muthu@egbsystems.com>
2018-10-30 19:47:08 +0530

    remove commented

--- app/controllers/KiteController.php
+++ app/controllers/KiteController.php
@@ -3,32 +3,6 @@
 class KiteController extends \BaseController {
 
 	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-        $calls = DB::table('calls')->get();
-        return View::make('stock.calllist')
-            ->with('calls', $calls);
-    }
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function getRunningCall()
-	{
-        $rCall = DB::table('calls')
-        ->where('status','=', 1)
-        ->get();
-       // print_r($rCall);
-        return json_encode($rCall);
-    }
-
-	/**
 	 * Display the specified resource.
 	 *
 	 * @param  int  $id
@@ -61,7 +35,6 @@ class KiteController extends \BaseController {
 			// exit;
 			//echo $v['tradingsymbol']." - $primaryTrend <br>";
 			$trend = $this->isTrendChange($sc[0], $sc[1], $v['tradingsymbol']);
-			// $opencall = $this->autoclose();
 			if($trend)
 				$c[] = $this->screenCall($v['tradingsymbol'], $v);
             if ($sc[0]) {
@@ -129,32 +102,6 @@ class KiteController extends \BaseController {
 		return $r;
     }
 	
-	
-	public function callWatch($callData, $data)
-	{
-		$target = 1;
-		$stop = -1;
-		if ($callData->call == 1) {
-			$diff =  (float)$data['change'] -  (float)$callData->per;
-		} else if ($callData->call == 2) {
-			$diff = (float)$callData->per -  (float)$data['change'];
-		}
-		//echo $callData->nse;
-		//echo  $callData->id."=>id".$callData->nse."=> Entry: ".$callData->per."=> CMP: ".$data['change']."=> Diff: $diff<br>";
-		if ($diff >= $target) {
-			if($data['diff'] < 0)
-			{
-			DB::table('intra_call')
-				->where('id', $callData->id)
-				->update(array('status' => 1, 'cPrice' => $data['lastPrice'], 'cPer' => $data['change']));
-			 }
-		} else if ($diff <= $stop) {
-			DB::table('intra_call')
-				->where('id', $callData->id)
-				->update(array('status' => -1, 'cPrice' => $data['lastPrice'], 'cPer' => $data['change']));
-		}
-	}
-
     public function insIntraCall($script, $price, $per, $call, $str, $in=null)
 	{
 		$i['nse'] = $script;
@@ -190,20 +137,6 @@ class KiteController extends \BaseController {
 				->where('id', $callData->id)
 				->update(array('status' => -1, 'cPrice' => $data['lastPrice'], 'cPer' => $data['change'], 'updated_on' => $u));
 		}
-
-		// if ($callData->call == 1) {
-		// 	$diff =  (float)$data['change'] -  (float)$callData->per;
-		// } else if ($callData->call == 2) {
-		// 	$diff = (float)$callData->per -  (float)$data['change'];
-		// }
-		// if ($diff > 0) {
-		// 	$status = 1;
-		// } else {
-		// 	$status = -1;
-		// }
-		// DB::table('intra_call')
-		// 	->where('id', $callData->id)
-		// 	->update(array('status' => $status, 'cPrice' => $data['lastPrice'], 'cPer' => $data['change']));
 		return $callData;
 	}
 	public function sma($script, $data)
@@ -230,23 +163,6 @@ class KiteController extends \BaseController {
 			$r = trader_rsi($s, $rsi);
 			$s1 = trader_sma($s, $sma1);
 			$s2 = trader_sma($s, $sma2);
-			// echo "<pre>"; print_r($s2);
-			// print_r($s1); print_r($r);
-			// echo "<pre>"; print_r($r); exit;
-            // array_unshift($a,"blue");
-            // array_pop($a);
-		// foreach($his as $key => $values) {
-		// 	$sum += $values->lastPrice;
-		// 	if ($i == $sma2) {
-		// 		$smaSum = $sum;
-		// 		$smaAvg2 = round(($sum / $sma2), 2);
-		// 	}
-		// 	if ($i == $sma1) {
-		// 		$smaAvg1 = round(($sum / $sma1), 2);
-		// 	}
-		// 	//echo "<pre>"; print_r($smaAvg1); print_r($smaAvg2); exit;
-		// $i++;
-		// }	
 		return array($s1[($sma1 - 1)], $s2[($sma1 - 1)], $r[($sma1 - 1)]);
 	}
 	public function isTrendChange($smaAvg1, $smaAvg2, $script)
@@ -304,16 +220,6 @@ class KiteController extends \BaseController {
 			$l = iterator_to_array($it, false);
 			if (count($l) == $sma3) {
 			$sma50 = trader_sma($l, $sma3);
-			// echo $sma50[($sma3 - 1)];
-			// echo $cPrice;
-			// exit;
-			// print_r($sma50);
-			// exit;
-			//print_r($last50); exit;
-			//foreach($last50 as $key => $values)
-			// 	$sum += $values->lastPrice;
-			// $smaAvg3 = round(($sum / $sma3), 2);
-			//if($smaAvg3 > $values->lastPrice)
 			if($sma50[($sma3 - 1)] > $cPrice)
 			{
 				return 'Downtrend';
@@ -345,8 +251,8 @@ class KiteController extends \BaseController {
 						->orderBy('id','DESC')
 						->get();
 					$comp = $comp[0];
-					print_r($c);
-					print_r($comp);
+					// print_r($c);
+					// print_r($comp);
 
                 //   $call = $c->
 				$dif = $comp->lastPrice - $c->price;
