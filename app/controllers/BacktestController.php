8054886e8af33d2a61853ac2cc21af8bd7fb686d
Muthu <v.muthamizh@gmail.com>
2018-11-11 11:15:06 +0530

    back

--- app/controllers/BacktestController.php
+++ app/controllers/BacktestController.php
@@ -33,7 +33,7 @@ class BacktestController extends \KiteController {
 		// 	// exit;
 		// }
 			Session::flush();
-			$this->runTest('INFIBEAM', $ldate);
+			$this->runTest('PCJEWELLER', $ldate);
 
 	}
 	public function runTest($script, $ldate)
