501b9c07f7eb61cc66aff6fa86629be80abb0761
Muthu <muthu@egbsystems.com>
2018-11-23 18:38:43 +0530

    screener

--- app/controllers/ScreenerController.php
+++ app/controllers/ScreenerController.php
@@ -10,18 +10,12 @@ class ScreenerController extends \BaseController {
 	public function index()
 	{
         return View::make('screener.screener');
-        // $calls = DB::table('calls')->get();
-        // return View::make('stock.calllist')
-        //     ->with('calls', $calls);
-        //$collection = collect(DB::table('kite_watch')->get());
     }
     public function screener($name=null)
 	{
 		if(!$name)
 			$name = 'ind_nifty50list';
 		$data = fopen("C:\\xampp\\htdocs\\market\\public\\data\\".$name.".csv", "r"); 
-        //echo "<pre>"; print_r($data); exit;
-        
         $sma1 = 21;
         $sma2 = 14;
         $sma3 = 80;
@@ -46,37 +40,13 @@ class ScreenerController extends \BaseController {
                 if(count($s) >= $sma3)
                 {
                     $r = trader_rsi($s, $rsi);
-                    // echo '<pre>'; print_r($r); exit();
                     $trend1 = $this->getSMA($s, $sma1);
                     $trend2 = $this->getSMA($s, $sma2);
                     $trend3 = $this->getSMA($s, $sma3);
-                  //echo "<pre>"; print_r($r);
-                    //echo "<pre>"; print_r($trend1); print_r($trend2);
-                    // if($s[($sma1-1)] <  $s2[($sma1-1)])
-                    //     echo "Up";
-                    // elseif($p['close'] > $p['sma2'])
-                    //     echo "Down"; 
-
                     array_push($arr, array('symbol' => $d[2], 'sma1' => $trend1, 'sma2' => $trend2, 'sma3' => $trend3, 'close' => $s[($sma3-1)], 'rsi' => $r[($sma3-1)]));
-                    //array_push($arr, array('symbol' => $d[2], 'sma1' => $s1[($sma1-1)], 'sma2' => $s2[($sma1-1)], 'close' => $s[($sma1-1)], 'rsi' => $r[($sma1-1)]));
-
-                } else {
-                    // echo $d[2]."<br>";
                 }
-               //echo "<pre>"; print_r($arr);
-                //echo $s1[20]. ">" .$s[20];
-                 //echo $s2[20]. ">" .$s[20]; 
-                // if($s1[20] > $s[20])
-                // {
-                //     return true;
-                // }
-                // elseif($s2[20] > $s[20])
-                // {
-                //     return false;
-                // } 
            }
-          // return json_encode($arr);  
-       return View::make('screener.screener')->with('arr',$arr);
+            return View::make('screener.screener')->with('arr',$arr);
         }
 
         function getSMA($ltp, $sma)
@@ -87,5 +57,4 @@ class ScreenerController extends \BaseController {
             elseif($ltp[($sma-1)] >  $s[($sma-1)])
                         return 'Down'; 
         }
-        //return array($l->SYMBOL, $value->CLOSEP, $s1, $s2);
     }
